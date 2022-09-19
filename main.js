0d3e45f33ef8eb0aa9f94daafc48c3f8636d9fcf
st-audrey <st_audrey@outlook.fr>
2022-09-19 17:23:04 +0200

    add style

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var message = document.getElementById("message");
-message.innerHTML = "hello"
+message.innerHTML = ""
 
 var counter = 0;
 var animation_time = "1000";
 var solution = document.getElementById("solution");
+solution.innerHTML = ""
 
 var disc_number_input = document.getElementById("disc_number");
 disc_number.value = null;
@@ -16,24 +17,22 @@ var stacks = {
 
 var res_history = []
 
-//welcome();
 renderBoard();
-function welcome(){
-    var body = document.getElementById("welcome");
-    body.classList.add("welcome");
 
-    setTimeout(() => {
-        var main = document.getElementById("main");
-        main.classList.add("main");
-        renderBoard();
-      }, "2000")
+// welcome();
 
+// function welcome(){
 
-}
+//     setTimeout(() => {
+//         var main = document.getElementById("main");
+//         main.classList.add("main");
+//         renderBoard();
+//       }, "2000")
+// }
 
 function renderBoard(){
     for(i = 1; i < 4 ; i++){
-        var container = document.getElementById("container");
+        var ipsos = document.getElementById("ipsos");
     
         var stack_container = document.createElement("div");
         stack_container.classList.add("stack-container");
@@ -42,23 +41,24 @@ function renderBoard(){
         happend_point.classList.add("happend-point");
         happend_point.setAttribute('id',"happend-point"+i)
     
-        container.appendChild(stack_container);
+        ipsos.appendChild(stack_container);
         stack_container.appendChild(happend_point);
     };
 }
-
 
 function checkInputValue(action_type){
 
     var disc_number_value = parseInt(disc_number_input.value);
     counter = 0;
+    message.removeAttribute("class");
 
     if(disc_number_value > 0 && disc_number_value <= 5){
-        
+        message.classList.add("txt-green");
+
         if(disc_number_value == 1){
-            message.innerHTML = "vous avez choisi 1 disque";
+            message.innerHTML = "Vous avez choisi 1 disque";
         }else{
-            message.innerHTML = "vous avez choisi " + disc_number_value + " disques";
+            message.innerHTML = "Vous avez choisi " + disc_number_value + " disques";
         }
 
         if(action_type == 'create'){
@@ -66,13 +66,13 @@ function checkInputValue(action_type){
             renderDisks();
             console.log(disc_number_value, stacks)
         }else if(action_type == 'resolve'){
-            runAlgoText(disc_number_value, 'start', 'end', 'mid');
-           renderAnimation();
-
+            runAlgo(disc_number_value, 'start', 'end', 'mid');
+            renderAnimation();
         }
 
     }else{
-        message.innerHTML = "merci de renseigner un chiffre entre 1 et 5";
+        message.innerHTML = "Merci de renseigner un chiffre entre 1 et 5";
+        message.classList.add("txt-red");
     }
 }
 
@@ -97,8 +97,6 @@ function createDisks(disc_number){
 }
 
 function renderDisks(){
-
-    console.log("stack", stacks);
 
     var happend_point1 = document.getElementById("happend-point1");
     var happend_point2 = document.getElementById("happend-point2");
@@ -137,10 +135,10 @@ function renderDisks(){
         disk.setAttribute('id',"disk"+stacks['end'][i]);
         happend_point3.appendChild(disk);
     }
- 
 }
 
 function renderAnimation(){
+
     setTimeout(() => {
         if(res_history.length == 0) {
             return;
@@ -150,6 +148,7 @@ function renderAnimation(){
         renderDisks();
         renderAnimation();
     }, animation_time);
+   
 }
 
 function moveDisk(disk_number, to){
@@ -161,19 +160,19 @@ function removeDisk(disk_number, from){
 }
 
 
-function runAlgoText(n, starting_stack, ending_stack, swap_stack){
+function runAlgo(n, starting_stack, ending_stack, swap_stack){
  
     if(n === 0){
         solution.innerHTML = "nombre de mouvements minimum = " + counter;
         return;
     }
 
-    runAlgoText(n-1, starting_stack, swap_stack, ending_stack);
+    runAlgo(n-1, starting_stack, swap_stack, ending_stack);
     counter ++;
     removeDisk(n, starting_stack);
     moveDisk(n, ending_stack);
     // deep copy of the board state
     res_history.push(JSON.parse(JSON.stringify(stacks)));
-    runAlgoText(n-1, swap_stack, ending_stack, starting_stack)
+    runAlgo(n-1, swap_stack, ending_stack, starting_stack)
     
 };
